2b6e66188df85b864f2bf0f41af898b7fc981653
jahman07104 <jahman@bellatlantic.net>
2021-01-26 16:01:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+function Header () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+    <h1>eve</h1>
+    </header>
+  )
+}
+
+
+function Main () {
+  return (
+    <section>
+    <p>main here</p>
+    </section>
   );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+    <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer year={new Date().getFullYear()}/>
+      </div>
+    );
+  }
 }
 
 export default App;
